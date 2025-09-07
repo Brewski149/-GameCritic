b71400da75c743013f9cc4d36b2e02e0a58e0624
Brewski149 <129285933+Brewski149@users.noreply.github.com>
2025-09-08 00:02:09 +0600

    Delete Gamecriticv3 directory

--- Gamecriticv3/test_simple.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "PHP is working!<br>";
-echo "Current time: " . date('Y-m-d H:i:s') . "<br>";
-echo "Server info: " . $_SERVER['HTTP_HOST'] . "<br>";
-echo "Request URI: " . $_SERVER['REQUEST_URI'] . "<br>";
-?>
